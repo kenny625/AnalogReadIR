444438a77267558cdf6728d77b611ad00c49e8d4
WeiChen <kenny625@gmail.com>
2014-02-15 23:15:15 +0800

    send JSON String

--- AnalogRead_IR.ino
+++ AnalogRead_IR.ino
@@ -1,7 +1,22 @@
-int sensorPin = A0;
-int sensorValue = 0;
-int sum = 0;
+int sensorPin1 = A0;
+int sensorPin2 = A1;
+int sensorPin3 = A2;
+int sensorPin4 = A3;
+int sensorValue1 = 0;
+int sensorValue2 = 0;
+int sensorValue3 = 0;
+int sensorValue4 = 0;
+int sum1 = 0;
+int sum2 = 0;
+int sum3 = 0;
+int sum4 = 0;
 int count = 0;
+String JSONString = "";
+String String1 = "{\"analogRead1\":\"";
+String String2 = "\", \"analogRead2\":\"";
+String String3 = "\", \"analogRead3\":\"";
+String String4 = "\", \"analogRead4\":\"";
+String String5 = "\"}";
 
 void setup()
 {
@@ -11,15 +26,25 @@ void setup()
 
 void loop()
 {
-  sensorValue = analogRead(sensorPin);
-  sum += sensorValue;
+  sensorValue1 = analogRead(sensorPin1);
+  sensorValue2 = analogRead(sensorPin2);
+  sensorValue3 = analogRead(sensorPin3);
+  sensorValue4 = analogRead(sensorPin4);
+  sum1 += sensorValue1;
+  sum2 += sensorValue2;
+  sum3 += sensorValue3;
+  sum4 += sensorValue4;
   delay(30);
 //  Serial.println(sensorValue);
   count++;
   if(count == 10){
     count = 0;
-    Serial.println(sum/10);
-    sum = 0;
+    JSONString = String1+ sum1/10 + String2 + sum2/10 + String3 + sum3/10 + String4 + sum4/10 + String5;
+    Serial.println(JSONString);
+    int sum1 = 0;
+    int sum2 = 0;
+    int sum3 = 0;
+    int sum4 = 0;
   }
   delay(30);      
 }
