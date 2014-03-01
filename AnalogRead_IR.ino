9158637407ad25ea4e9dc1f5e930045e5864b72b
WeiChen <kenny625@gmail.com>
2014-03-01 16:02:24 +0800

    dynamic sensor number

--- AnalogRead_IR.ino
+++ AnalogRead_IR.ino
@@ -1,112 +1,120 @@
-int sensorPin1 = A0;
-int sensorPin2 = A1;
-int sensorPin3 = A2;
-int sensorPin4 = A4;
-int sensorValue1 = 0;
-int preSensorValue1 = 0;
-int filteredSensorValue1 = 0;
-int sensorValue2 = 0;
-int preSensorValue2 = 0;
-int filteredSensorValue2 = 0;
-int sensorValue3 = 0;
-int preSensorValue3 = 0;
-int filteredSensorValue3 = 0;
-int sensorValue4 = 0;
-int preSensorValue4 = 0;
-int filteredSensorValue4 = 0;
-int sum1 = 0;
-int sum2 = 0;
-int sum3 = 0;
-int sum4 = 0;
+int sensorPin[10];
+int sensorValue[10];
+int preSensorValue[10];
+int filteredSensorValue[10];
+int sum[10];
+int maxValue[10];
+int minValue[10];
+int numSensors = 2;
 int count = 0;
 String JSONString = "";
-String String1 = "{\"analogRead1\":\"";
-String String2 = "\", \"analogRead2\":\"";
-String String3 = "\", \"analogRead3\":\"";
-String String4 = "\", \"analogRead4\":\"";
-String String5 = "\"}";
+String outputString[10];
+String endString = "\"}";
 int alpha = 0.5;
-int maxValue1 = 0;
-int minValue1 = 9999;
-int maxValue2 = 0;
-int minValue2 = 9999;
-int maxValue3 = 0;
-int minValue3 = 9999;
-int maxValue4 = 0;
-int minValue4 = 9999;
 
 void setup()
 {
   Serial.begin(9600);
 //  analogReference(INTERNAL2V56);
+  sensorPin[0] = A0;
+  sensorPin[1] = A1;
+  sensorPin[2] = A2;
+  sensorPin[3] = A3;
+  sensorPin[4] = A4;
+  sensorPin[5] = A5;
+  sensorPin[6] = A6;
+  sensorPin[7] = A7;
+  sensorPin[8] = A8;
+  sensorPin[9] = A9;
+  outputString[0] = "{\"analogRead1\":\"";
+  outputString[1] = "\", \"analogRead2\":\"";
+  outputString[2] = "\", \"analogRead3\":\"";
+  outputString[3] = "\", \"analogRead4\":\"";
+  outputString[4] = "\", \"analogRead5\":\"";
+  outputString[5] = "\", \"analogRead6\":\"";
+  outputString[6] = "\", \"analogRead7\":\"";
+  outputString[7] = "\", \"analogRead8\":\"";
+  outputString[8] = "\", \"analogRead9\":\"";
+  outputString[9] = "\", \"analogRead10\":\"";
+  initArray();
 }
 
 void loop()
 {
-  
-  sensorValue1 = analogRead(sensorPin1);
-  sensorValue2 = analogRead(sensorPin2);
-  sensorValue3 = analogRead(sensorPin3);
-  sensorValue4 = analogRead(sensorPin4);
-  if(preSensorValue1 == 0) preSensorValue1 = sensorValue1;
-  if(preSensorValue2 == 0) preSensorValue2 = sensorValue2;
-  if(preSensorValue3 == 0) preSensorValue3 = sensorValue3;
-  if(preSensorValue4 == 0) preSensorValue4 = sensorValue4;
-  filteredSensorValue1 = sensorValue1 / 10 * alpha + (preSensorValue1 * (1 - alpha));
-  filteredSensorValue2 = sensorValue2 / 10 * alpha + (preSensorValue2 * (1 - alpha));
-  filteredSensorValue3 = sensorValue3 / 10 * alpha + (preSensorValue3 * (1 - alpha));
-  filteredSensorValue4 = sensorValue4 / 10 * alpha + (preSensorValue4 * (1 - alpha));
-  preSensorValue1 = sensorValue1;
-  preSensorValue2 = sensorValue2;
-  preSensorValue3 = sensorValue3;
-  preSensorValue4 = sensorValue4;
-  if(filteredSensorValue1 > maxValue1){
-    maxValue1 = filteredSensorValue1;
-  }else if(filteredSensorValue1 < maxValue1){
-    minValue1 = filteredSensorValue1;
-  } 
-  if(filteredSensorValue2 > maxValue2){
-    maxValue2 = filteredSensorValue2;
-  }else if(filteredSensorValue2 < maxValue2){
-    minValue2 = filteredSensorValue2;
+  int i;
+  for(i=0;i<numSensors;i++){
+    sensorValue[i] = analogRead(sensorPin[i]);
+    if(preSensorValue[i] == 0) preSensorValue[i] = sensorValue[i];
+    filteredSensorValue[i] = sensorValue[i] / 10 * alpha + (preSensorValue[i] * (1 - alpha));
+    preSensorValue[i] = sensorValue[i];
+    if(filteredSensorValue[i] > maxValue[i]){
+      maxValue[i] = filteredSensorValue[i];
+    }else if(filteredSensorValue[i] < maxValue[i]){
+      minValue[i] = filteredSensorValue[i];
+    }
+    sum[i] += filteredSensorValue[i];
   }
-  if(filteredSensorValue3 > maxValue3){
-    maxValue3 = filteredSensorValue3;
-  }else if(filteredSensorValue3 < maxValue3){
-    minValue3 = filteredSensorValue3;
-  }
-  if(filteredSensorValue4 > maxValue4){
-    maxValue4 = filteredSensorValue4;
-  }else if(filteredSensorValue4 < maxValue4){
-    minValue4 = filteredSensorValue4;
-  }
-  
-  
-  sum1 += filteredSensorValue1;
-  sum2 += filteredSensorValue2;
-  sum3 += filteredSensorValue3;
-  sum4 += filteredSensorValue4;
+//  sensorValue1 = analogRead(sensorPin1);
+//  sensorValue2 = analogRead(sensorPin2);
+//  sensorValue3 = analogRead(sensorPin3);
+//  sensorValue4 = analogRead(sensorPin4);
+//  if(preSensorValue1 == 0) preSensorValue1 = sensorValue1;
+//  if(preSensorValue2 == 0) preSensorValue2 = sensorValue2;
+//  if(preSensorValue3 == 0) preSensorValue3 = sensorValue3;
+//  if(preSensorValue4 == 0) preSensorValue4 = sensorValue4;
+//  filteredSensorValue1 = sensorValue1 / 10 * alpha + (preSensorValue1 * (1 - alpha));
+//  filteredSensorValue2 = sensorValue2 / 10 * alpha + (preSensorValue2 * (1 - alpha));
+//  filteredSensorValue3 = sensorValue3 / 10 * alpha + (preSensorValue3 * (1 - alpha));
+//  filteredSensorValue4 = sensorValue4 / 10 * alpha + (preSensorValue4 * (1 - alpha));
+//  preSensorValue1 = sensorValue1;
+//  preSensorValue2 = sensorValue2;
+//  preSensorValue3 = sensorValue3;
+//  preSensorValue4 = sensorValue4;
+//  if(filteredSensorValue1 > maxValue1){
+//    maxValue1 = filteredSensorValue1;
+//  }else if(filteredSensorValue1 < maxValue1){
+//    minValue1 = filteredSensorValue1;
+//  } 
+//  if(filteredSensorValue2 > maxValue2){
+//    maxValue2 = filteredSensorValue2;
+//  }else if(filteredSensorValue2 < maxValue2){
+//    minValue2 = filteredSensorValue2;
+//  }
+//  if(filteredSensorValue3 > maxValue3){
+//    maxValue3 = filteredSensorValue3;
+//  }else if(filteredSensorValue3 < maxValue3){
+//    minValue3 = filteredSensorValue3;
+//  }
+//  if(filteredSensorValue4 > maxValue4){
+//    maxValue4 = filteredSensorValue4;
+//  }else if(filteredSensorValue4 < maxValue4){
+//    minValue4 = filteredSensorValue4;
+//  }
+//  
+//  
+//  sum1 += filteredSensorValue1;
+//  sum2 += filteredSensorValue2;
+//  sum3 += filteredSensorValue3;
+//  sum4 += filteredSensorValue4;
   delay(30);
 //  Serial.println(sensorValue4);
   count++;
   if(count == 10){
     count = 0;
-    JSONString = String1+ (sum1-maxValue1-minValue1)/8 + String2 + (sum2-maxValue2-minValue2)/8 + String3 + (sum3-maxValue3-minValue3)/8 + String4 + (sum4-maxValue4-minValue4)/8 + String5;
-    double z = (double)(sum1-maxValue1-minValue1)/8/1024*5;
+    JSONString = "";
+    for(i=0;i<numSensors;i++){
+      JSONString += outputString[i];
+      JSONString += (sum[i]-maxValue[i]-minValue[i])/8;
+      sum[i] = 0;
+      maxValue[i] = 0;
+      minValue[i] = 9999;
+    }
+    JSONString += endString;
+    
+//    JSONString = String1+ (sum1-maxValue1-minValue1)/8 + String2 + (sum2-maxValue2-minValue2)/8 + String3 + (sum3-maxValue3-minValue3)/8 + String4 + (sum4-maxValue4-minValue4)/8 + String5;
+//    double z = (double)(sum1-maxValue1-minValue1)/8/1024*5;
 //    printDouble(z,10);
     Serial.println(JSONString);
-    sum1 = 0;
-    sum2 = 0;
-    sum3 = 0;
-    sum4 = 0;
-    maxValue1 = 0;
-    minValue1 = 9999;
-    maxValue2 = 0;
-    minValue2 = 9999;
-    maxValue3 = 0;
-    minValue3 = 9999;
-    maxValue4 = 0;
-    minValue4 = 9999;
   }
   delay(30);      
 }
@@ -136,6 +144,18 @@ void printDouble( double val, byte precision){
     while(  padding--)
       Serial.print("0");
     Serial.println(frac,DEC) ;
+  }
+}
+
+void initArray(){
+  int i;
+  for(i=0;i<numSensors;i++){
+    sensorValue[i] = 0;
+    preSensorValue[i] = 0;
+    filteredSensorValue[i] = 0;
+    sum[i] = 0;
+    maxValue[i] = 0;
+    minValue[i] = 9999;
   }
 }
 
